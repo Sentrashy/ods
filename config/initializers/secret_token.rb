97c83130a76572d8cb5fc9f8bebae3a7b264b337
Sentrashy <domain.major@gmail.com>
2012-03-21 23:13:35 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ods::Application.config.secret_token = '2550361e97e6fc87540104ab1a6ea0752d575eb24a5505ac475b22861641f560eb781b6750cba2d7fd0b69d6bf728f43d16c787195d8e8a1b44f9b007b85fbbf'
